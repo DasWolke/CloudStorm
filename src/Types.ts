753d20e01d3cf9637492f01810cf8366980476bf
Papa <bradleyjplatt@yahoo.com>
2022-04-11 14:21:51 -0700

    Update IntentFlags type to match exports
    Closes #18

--- src/Types.ts
+++ src/Types.ts
@@ -1,20 +1,4 @@
-export interface IntentFlags {
-	GUILDS: number;
-	GUILD_MEMBERS: number;
-	GUILD_BANS: number;
-	GUILD_EMOJIS_AND_STICKERS: number;
-	GUILD_INTEGRATIONS: number;
-	GUILD_WEBHOOKS: number;
-	GUILD_INVITES: number;
-	GUILD_VOICE_STATES: number;
-	GUILD_PRESENCES: number;
-	GUILD_MESSAGES: number;
-	GUILD_MESSAGE_REACTIONS: number;
-	GUILD_MESSAGE_TYPING: number
-	DIRECT_MESSAGES: number;
-	DIRECT_MESSAGE_REACTIONS: number
-	DIRECT_MESSAGE_TYPING: number;
-}
+export type IntentFlags = (typeof import("./Intents"))["flags"];
 
 export type IntentResolvable = number | Array<number> | keyof IntentFlags | Array<keyof IntentFlags>;
 
