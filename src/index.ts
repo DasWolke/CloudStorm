56a855a929301a43250d79a2fc0f388f3476812a
Papa <bradleyjplatt@yahoo.com>
2023-05-19 19:27:38 -0700

    Export BetterWS for Discord like connections

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ import Intents = require("./Intents");
 import Shard = require("./Shard");
 import ShardManager = require("./ShardManager");
 
-export { Client, Constants, Intents, Shard, ShardManager };
+import BetterWs = require("./structures/BetterWs");
+
+export { BetterWs, Client, Constants, Intents, Shard, ShardManager };
 export * from "./Types";
