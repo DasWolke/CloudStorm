3607e8393aa8e0fefbba0540b127bf33694b2a21
Cadence Ember <cadence@disroot.org>
2026-05-23 22:50:52 +1200

    Fixes (#24)
    * Do not restart heartbeat during user_close
    
    * Retry connecting if it does not respond at all
    
    * Make signal parameter last in eventSwitch
    
    * Update new usage of eventSwitch abort timer
    
    ---------
    
    Co-authored-by: Brad <bradleyjplatt@yahoo.com>

--- src/eventSwitch.ts
+++ src/eventSwitch.ts
@@ -5,15 +5,22 @@ import type { EventEmitter } from "events";
  * If the callback returns a promise, waits for the promise to resolve or reject. eventSwitch will resolve or reject with the same value.
  * All added listeners are removed before eventSwitch returns.
  */
-function eventSwitch(emitter: EventEmitter, cbs: { [eventName: string]: (...args: any[]) => any }): Promise<void> {
+function eventSwitch(emitter: EventEmitter, cbs: { [eventName: string]: (...args: any[]) => any }, signal?: AbortSignal | null): Promise<void> {
 	const realListeners = new Map<string, (...args: Array<any>) => void>();
-	return Promise.race(Object.entries(cbs).map(([event, cb]) => {
-		return new Promise<void>((resolve, reject) => {
-			const l = (...args: Array<any>) => (async () => cb(...args))().then(resolve, reject);
-			realListeners.set(event, l);
-			emitter.once(event, l);
-		});
-	})).finally(() => {
+	return Promise.race(
+		Object.entries(cbs).map(([event, cb]) => {
+			return new Promise<void>((resolve, reject) => {
+				const l = (...args: Array<any>) => (async () => cb(...args))().then(resolve, reject);
+				realListeners.set(event, l);
+				emitter.once(event, l);
+			});
+		}).concat(new Promise<void>((resolve, reject) => {
+			signal?.throwIfAborted()
+			signal?.addEventListener("abort", () => {
+				reject(signal.reason)
+			}, {once: true})
+		}))
+	).finally(() => {
 		for (const [event, l] of realListeners.entries()) {
 			emitter.removeListener(event, l);
 		}
